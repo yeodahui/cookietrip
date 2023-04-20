041fcbba532787f4ddabf6a918a9c47eae3849db
여다희 <yeodahui.dev@gmail.com>
2023-04-21 05:20:14 +0900

    스타일: 프리텐다드 폰트 설치

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -13,12 +13,18 @@ html {
 }
 
 body {
+  margin: 0;
   width: 100vw;
   height: 100vh;
   display: flex;
   font-size: ${({ theme }) => theme.fontSizes.base};
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, "Segoe UI",
+  "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+  "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   background-color: ${({ theme }) => theme.colors.background};
-  
+
   #root {
     width: 100%;
     min-height: 100vh;
